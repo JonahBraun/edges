5bbb8f999b9f0211661d224104a5014046e08966
Jonah Braun <Jonah@goodcoffee.ca>
2016-07-14 11:57:13 -0600

    send second class documentation

--- src/return_send.go
+++ src/return_send.go
@@ -1,12 +1,37 @@
-// you can't return a send
+/*
+Channel Sending is Second Class
+
+Sending on a channel does not return a value (not even nil). This means it can
+not be assigned, passed or returned.
+
+This program does not compile.
+*/
+
 package main
 
 import "fmt"
 
+func inspect(v interface{}) {
+	fmt.Printf("%T %#v \n", v, v)
+}
+
 func foo() {
 	a := make(chan bool)
+
+	// Fail:
+	b := a<-true
+	//= a <- true used as value
+
+	// Fail:
+	inspect(a<-true)
+	//= a <- true used as value
+
+	// Fail:
 	return a<-true
+	//= a <- true used as value
+	//= too many arguments to return
 }
+
 func main() {
-	fmt.Println(foo())
+	foo()
 }
